0dc2d31ebe6f3eab3c28ba8a9fdc909d3e398948
pabricioLopes <33148737+pabricioLopes@users.noreply.github.com>
2020-06-28 17:25:56 -0300

    Create Plot3.R

---
+++ Plot3.R
@@ -0,0 +1,42 @@
+# Making Plot 3 - dateTime versus Energy sub metering
+
+# Loads and adds the package "data.table"
+
+library("data.table")
+
+# Returns the current directory before the change, invisibly and with 
+# the same conventions as getwd.
+
+setwd("C:/Users/Pabricio Marcos/Desktop/Coursera/curso")
+
+# Reads the data GAP (Global active power) from file then subsets data for specified 
+# dates
+
+GAP <- data.table::fread(input = "household_power_consumption.txt"
+                             , na.strings="?"
+)
+
+# Prevents Scientific Notation
+
+GAP[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+
+# Making a POSIXct date capable of being filtered and graphed by time of day
+
+GAP[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
+
+# Filter Dates for 2007-02-01 and 2007-02-02
+
+GAP <- GAP[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
+
+png("plot3.png", width=480, height=480)
+
+# Plot 3
+plot(GAP[, dateTime], GAP[, Sub_metering_1], type="l", xlab="", ylab="Energy sub metering")
+lines(GAP[, dateTime], GAP[, Sub_metering_2],col="red")
+lines(GAP[, dateTime], GAP[, Sub_metering_3],col="blue")
+legend("topright"
+       , col=c("black","red","blue")
+       , c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  ")
+       ,lty=c(1,1), lwd=c(1,1))
+
+dev.off()
